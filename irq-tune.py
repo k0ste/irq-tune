4c62b9d2a8905db5d98be409b8b49715f298ad2a
Ian Gable <igable@uvic.ca>
2014-11-07 09:17:53 -0800

    remove commented out junk

--- irq-tune.py
+++ irq-tune.py
@@ -1,6 +1,13 @@
 #!/bin/python
 
+# irq-tune
+#
+# Author: Ian Gable <igable@uvic.ca>
+#
 # https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Performance_Tuning_Guide/s-cpu-irq.html
+#
+#
+
 import subprocess
 import json
 import shlex
@@ -15,7 +22,6 @@ def get_mask(selectedcpu):
       cpumask = "0" + cpumask
   return cpumask
 
-
 def add_commas(mask):
   split_mask = [mask[x:x+8] for x in range(0,len(mask),8)]
   comma_sep =""
@@ -26,17 +32,14 @@ def add_commas(mask):
       comma_sep = comma_sep + "," + split_mask[i]
   return comma_sep
  
-
 def total_mask(cpulist, masktype ):
   masklist = []
   formatted_mask =""
   for cpu in cpulist:
     masklist.append(get_mask(cpu))
-  #print masklist
   ormask = int(0)
   for mask in masklist:
     ormask = ormask | int(mask,2)
-  #print format(int('11111111',2),'010x')
   if masktype == "binary":
     formatted_mask = format(ormask,'048b')
   else:
@@ -59,36 +62,26 @@ def write_proc(irqlist):
 
 def main():
 
-  irqlist = [ { 'irq': '259', 'cores': [32] },\
-              { 'irq': '370', 'cores': [36]      } ]
-  cpulist = [0,1,2,3]
-
-  bmask = total_mask(cpulist,"binary")
-  hmask1 = total_mask(cpulist,"hex")
-  hmask2 = total_mask(irqlist[0]['cores'],"hex")
-  hreal = total_mask([36],"hex")
-  #print hmask1
-  #print hmask2
-  #print hreal
+  # Example irq data structure
+  #irqlist = [ { 'irq': '259', 'cores': [32,21] },\
+  #            { 'irq': '370', 'cores': [36]      } ]
 
   json_data = open("irq.json").read()
   irqdict = json.loads(json_data)
 
-  #write_proc(irqlist)
   write_proc(irqdict)
-  #print(test_load)
  
-    #bash_command = "echo \"%s\" > /proc/irq/%s/smp_affinity" % (irqmask, irqnumber)
-    #bash_command = "echo 'hello%s' > junk" % ( "doggy")
-    #bash_test = "echo 'irq_mask: %s irq_number: %s' " % (irqmask, irqnumber)
-    #print bash_command
-    #print shlex.split(bash_command),
-    #process = subprocess.Popen(shlex.split(bash_command), stdout=subprocess.PIPE)
-    #process = subprocess.Popen([r"echo","'hollow blix'"], stdout=open("/root/irq/junk",'w'))
-    #output = process.communicate()[0]
-    #print output
-
-
 
 if __name__ == "__main__":
   main()
+
+# Some debugging junk
+#
+#  bmask = total_mask(cpulist,"binary")
+#  hmask1 = total_mask(cpulist,"hex")
+#  hmask2 = total_mask(irqlist[0]['cores'],"hex")
+#  hreal = total_mask([36],"hex")
+#  print hmask2
+#  print hreal
+
+
