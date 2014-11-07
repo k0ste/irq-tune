28943d2ac9583ccfc96496a9029a785f454f0b42
Ian Gable <igable@uvic.ca>
2014-11-07 09:36:25 -0800

    autopep8 the code

--- irq-tune.py
+++ irq-tune.py
@@ -1,9 +1,11 @@
 #!/bin/python
+# vim: set expandtab ts=4 sw=4:
 
 # irq-tune
 #
 # Author: Ian Gable <igable@uvic.ca>
 #
+# Read this to understand more about what this script does:
 # https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Performance_Tuning_Guide/s-cpu-irq.html
 #
 #
@@ -12,68 +14,74 @@ import subprocess
 import json
 import shlex
 
+
 def get_mask(selectedcpu):
 
-  cpumask =""
-  for cpu in range (0, 48):
-    if(cpu == selectedcpu):
-      cpumask = "1" + cpumask
-    else:
-      cpumask = "0" + cpumask
-  return cpumask
+    cpumask = ""
+    for cpu in range(0, 48):
+        if(cpu == selectedcpu):
+            cpumask = "1" + cpumask
+        else:
+            cpumask = "0" + cpumask
+    return cpumask
+
 
 def add_commas(mask):
-  split_mask = [mask[x:x+8] for x in range(0,len(mask),8)]
-  comma_sep =""
-  for i in range(0,len(split_mask)):
-    if (i < 1):
-      comma_sep = split_mask[i]
+
+    split_mask = [mask[x:x + 8] for x in range(0, len(mask), 8)]
+    comma_sep = ""
+    for i in range(0, len(split_mask)):
+        if (i < 1):
+            comma_sep = split_mask[i]
+        else:
+            comma_sep = comma_sep + "," + split_mask[i]
+    return comma_sep
+
+
+def total_mask(cpulist, masktype):
+    
+    masklist = []
+    formatted_mask = ""
+    for cpu in cpulist:
+        masklist.append(get_mask(cpu))
+    ormask = int(0)
+    for mask in masklist:
+        ormask = ormask | int(mask, 2)
+    if masktype == "binary":
+        formatted_mask = format(ormask, '048b')
     else:
-      comma_sep = comma_sep + "," + split_mask[i]
-  return comma_sep
- 
-def total_mask(cpulist, masktype ):
-  masklist = []
-  formatted_mask =""
-  for cpu in cpulist:
-    masklist.append(get_mask(cpu))
-  ormask = int(0)
-  for mask in masklist:
-    ormask = ormask | int(mask,2)
-  if masktype == "binary":
-    formatted_mask = format(ormask,'048b')
-  else:
-    formatted_mask = format(ormask,'016x')
-  return add_commas(formatted_mask)
+        formatted_mask = format(ormask, '016x')
+    return add_commas(formatted_mask)
+
 
 def write_proc(irqlist):
 
-  for irq in irqlist:
-    irqmask = total_mask(irq['cores'],"hex")
-    irqnumber = irq['irq']
-    procname = "/proc/irq/%s/smp_affinity" % ( irqnumber )
-    procfile = open(procname,'w')
-    procfile.write(irqmask)
-    procfile.close()
+    for irq in irqlist:
+        irqmask = total_mask(irq['cores'], "hex")
+        irqnumber = irq['irq']
+        procname = "/proc/irq/%s/smp_affinity" % (irqnumber)
+        procfile = open(procname, 'w')
+        procfile.write(irqmask)
+        procfile.close()
 
-    print "Set IRQ:%s to cores: %s" % ( irqnumber , ', '.join(str(x) for x in irq['cores']) )
-    print "mask: %s procfile: %s" % (irqmask, procname)
- 
+        print "Set IRQ:%s to cores: %s" % (irqnumber, ', '.join(str(x) for x in irq['cores']))
+        print "mask: %s procfile: %s" % (irqmask, procname)
+
 
 def main():
 
-  # Example irq data structure
-  #irqlist = [ { 'irq': '259', 'cores': [32,21] },\
-  #            { 'irq': '370', 'cores': [36]      } ]
+    # Example irq data structure
+    # irqlist = [ { 'irq': '259', 'cores': [32,21] },\
+    #            { 'irq': '370', 'cores': [36]      } ]
 
-  json_data = open("irq.json").read()
-  irqdict = json.loads(json_data)
+    json_data = open("irq.json").read()
+    irqdict = json.loads(json_data)
 
-  write_proc(irqdict)
- 
+    write_proc(irqdict)
+
 
 if __name__ == "__main__":
-  main()
+    main()
 
 # Some debugging junk
 #
@@ -83,5 +91,3 @@ if __name__ == "__main__":
 #  hreal = total_mask([36],"hex")
 #  print hmask2
 #  print hreal
-
-
